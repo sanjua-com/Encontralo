1ed3c28410d8195d48bf4dd615ed9df6b01eaf70
Matias Torres <info@sanjua.com>
2017-08-10 18:28:29 -0300

    Add map, and hide options categories

--- sites/all/themes/basic/js/build/scripts.js
+++ sites/all/themes/basic/js/build/scripts.js
@@ -1,2 +1,36 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
 
-//# sourceMappingURL=scripts.map
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function () {
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.hidden = {
+
+    attach: function (context, settings) {
+
+      jQuery('.hierarchical-select select:first-child').change(function(){
+        if(jQuery(this).val() == 5){ // or this.value == 'volvo'
+          jQuery('.fieldset-vehiculos').show();
+          jQuery('.fieldset-propiedades').hide();
+        }
+        else if(jQuery(this).val() == 6){ // or this.value == 'volvo'
+          jQuery('.fieldset-propiedades').show();
+          jQuery('.fieldset-vehiculos').hide();
+        } else {
+          jQuery('.fieldset-vehiculos').hide();
+          jQuery('.fieldset-propiedades').hide();
+        }
+      });
+    }
+  };
+
+} (jQuery));
